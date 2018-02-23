c7b168a3e2af50ef5d7e950186a4febf14418c21
Jegatheesh <jegatheesh93@gmail.com>
2018-02-23 14:07:56 +0400

    Local date time basics

--- testSpring/src/java/springDemo/test/java8/DateTimeBasics.java
+++ testSpring/src/java/springDemo/test/java8/DateTimeBasics.java
@@ -1,12 +1,23 @@
 package springDemo.test.java8;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class DateTimeBasics {
 
 	public static void main(String[] args) {
-		LocalDate ld = LocalDate.now();
-		System.out.println(ld);
+		
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+		
+		LocalDate start = LocalDate.from(dtf.parse("01-01-2018"));
+		System.out.println(start);
+		
+		LocalDate now = LocalDate.now();
+		System.out.println(now);
+		
+		System.out.println(ChronoUnit.DAYS.between(start, now));
+		
 	}
 
 }
